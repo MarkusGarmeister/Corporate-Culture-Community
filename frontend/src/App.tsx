14f638fee836e19384cb9276b1e91f768f99f5a9
MarkusGarmeister <markusgarmeister@gmail.com>
2025-10-19 16:49:29 +0200

    feat: implement authProvider for authentication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,12 @@
-import { Admin } from "react-admin";
+import { Admin, Ressource } from "react-admin";
 import { Layout } from "./Layout";
+import { authProvider } from "./auth/authProvider";
 
-export const App = () => <Admin layout={Layout}></Admin>;
+export const App = () => (
+  <Admin layout={Layout} authProvider={authProvider}>
+    <Ressource
+      name="venues"
+      list={() => <div>Venue Marketplace</div>}
+    ></Ressource>
+  </Admin>
+);
---
+++ frontend/src/auth/authProvider.ts
@@ -0,0 +1,39 @@
+import { AuthProvider } from "react-admin";
+import axios from "axios";
+
+const API_URL = "http://localhost:8000/api/v1";
+
+export const authProvider: AuthProvider = {
+  login: async ({ username, password }) => {
+    try {
+      const response = await axios.post(`${API_URL}/auth/login`, {
+        email: username,
+        password,
+      });
+      const jwt_token = response.data.access_token;
+      localStorage.setItem("token", jwt_token);
+      return Promise.resolve();
+    } catch (error) {
+      console.log(error);
+      return Promise.reject();
+    }
+  },
+
+  logout: async () => {
+    localStorage.removeItem("token");
+    return Promise.resolve();
+  },
+
+  checkAuth: async () => {
+    if (!localStorage.getItem("token")) {
+      throw new Error("Authentication is required");
+    }
+  },
+  checkError: async (error) => {
+    if (error.status === 401 || error.status === 403) {
+      localStorage.removeItem("token");
+      throw new Error("Session expired");
+    }
+    return Promise.resolve();
+  },
+};
