31091301327513a177c17f55ed57dbc6f61be2db
MarkusGarmeister <markusgarmeister@gmail.com>
2025-11-10 14:30:31 +0100

    feat: refactor LocationCreate component to use AddLocation for venue creation

--- frontend/src/pages/location/LocationCreate.tsx
+++ frontend/src/pages/location/LocationCreate.tsx
@@ -1,38 +1,15 @@
-import {
-  Create,
-  SimpleForm,
-  TextInput,
-  NumberInput,
-  required,
-} from "react-admin";
+import { Create } from "react-admin";
+import { AddLocation } from "../AddLocation";
 
 export const LocationCreate = () => (
-  <Create>
-    <SimpleForm>
-      <TextInput source="name" validate={[required()]} fullWidth />
-      <TextInput
-        source="address_line_1"
-        label="Address Line 1"
-        validate={[required()]}
-        fullWidth
-      />
-      <TextInput
-        source="address_line_2"
-        label="Address Line 2 (Optional)"
-        fullWidth
-      />
-      <TextInput source="city" validate={[required()]} />
-      <TextInput source="state" validate={[required()]} />
-      <TextInput source="zip_code" label="Zip Code" validate={[required()]} />
-      <TextInput source="country" validate={[required()]} />
-      <NumberInput source="capacity" validate={[required()]} />
-      <NumberInput
-        source="price_range"
-        label="Price Range (1-5)"
-        validate={[required()]}
-        min={1}
-        max={5}
-      />
-    </SimpleForm>
+  <Create
+    sx={{
+      "& .RaCreate-main": {
+        marginTop: 0,
+        paddingTop: 0,
+      },
+    }}
+  >
+    <AddLocation />
   </Create>
 );
